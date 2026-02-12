9e17c9efe5160e337f6c24640eb54b5d54b26218
rudyanta.alfeus@gmail.com <rudyanta.alfeus@gmail.com>
2026-02-12 20:25:59 +0700

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
+import { BrowserRouter, Route, Outlet, Routes } from 'react-router-dom';
+
+const AuthLayout = () => {
+  return (
+    <div className='flex min-h-screen flex-col bg-black'>
+      <Outlet />
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className='flex min-h-screen flex-col bg-black'>
+      <header />
+      <Outlet />
+    </div>
+  );
+};
+
 export const App = () => {
   return (
-    <div>
-      <p className='text-md-bold text-blue-400'>Hello</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Auth routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/login' element={<div />} />
+          <Route path='/register' element={<div />} />
+        </Route>
+
+        {/* App routes (with header) */}
+        <Route element={<AppLayout />}>
+          <Route path='/' element={<div />} />
+          <Route path='/search' element={<div />} />
+          <Route path='/my-profile' element={<div />} />
+          <Route path='/add-post' element={<div />} />
+          <Route path='/update-profile' element={<div />} />
+          <Route path='/profile/:username' element={<div />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
